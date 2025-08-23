37081b62027d9fbdbc4c7f502ff4549ef12b78fe
unknown <juan.rrodriguez@labinfo.is.escuelaing.edu.co>
2025-08-23 08:58:08 -0500

    funcion de hilos

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # -concurrent-prog-dogs-race-master-ARSW
 ![alt text](image.png)
 ![alt text](image-1.png)
+
+con 3 hilos 
+
+![alt text](image-2.png)
--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -2,13 +2,35 @@ package edu.eci.arsw.primefinder;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 50_000_000);
-		
-		pft.start();
-		
-		
-	}
-	
+    public static void main(String[] args) {
+        int totalDatos = 100_000_000;
+        int numHilos = 100;
+
+        crearHilos(totalDatos, numHilos);
+    }
+
+    public static void crearHilos(int totalDatos, int numHilos) {
+        int chunkSize = totalDatos / numHilos;
+        Thread[] hilos = new Thread[numHilos];
+
+        for (int i = 0; i < numHilos; i++) {
+            int inicio = i * chunkSize;
+            int fin = (i == numHilos - 1) ? totalDatos : inicio + chunkSize;
+
+            hilos[i] = new PrimeFinderThread(inicio, fin);
+            hilos[i].start();
+        }
+
+        for (int i = 0; i < numHilos; i++) {
+            try {
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Todos los hilos han terminado.");
+    }
 }
+
+
